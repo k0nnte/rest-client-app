1b866b63f3093aca7218449ec6e81c2bc3025f62
binkki <maomao82@naver.com>
2025-04-02 17:43:49 +0400

    feat: add language switcher

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,11 +2,13 @@ import { NavLink } from 'react-router';
 import { useTranslation } from 'react-i18next';
 
 export default function MainPage() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   return (
     <>
       <div>{t('hi')}</div>
+      <button onClick={() => i18n.changeLanguage('ru')}>RU</button>
+      <button onClick={() => i18n.changeLanguage('en')}>EN</button>
       <NavLink to="/signin">Sign In</NavLink>
       <NavLink to="/signup">Sign Up</NavLink>
       <NavLink to="/client">Rest API client</NavLink>
