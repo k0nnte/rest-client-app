714e0c5031c4f4c53f6ec76c7a9a3cda73ec6dec
k0nnte <bystrov.igor25@gmail.com>
2025-04-04 03:37:25 +0300

    feat: add not authorized nain page and test

--- src/components/anonim/Anonim.tsx
+++ src/components/anonim/Anonim.tsx
@@ -1,10 +1,17 @@
+import { NavLink } from 'react-router';
+import './Anonim.scss';
+
 const Anonim = () => {
   return (
-    <div>
+    <div className="wrapper_anon_main">
       <p>Welcome</p>
-      <div>
-        <button>Sign In</button>
-        <button>Sign Up</button>
+      <div className="btn_wrapper">
+        <NavLink to="/signin">
+          <button>Sign In</button>
+        </NavLink>
+        <NavLink to="/signup">
+          <button>Sign Up</button>
+        </NavLink>
       </div>
     </div>
   );
