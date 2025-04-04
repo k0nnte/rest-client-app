5fd6f218ce41681bfa3f83c8b72d3b84f45da525
k0nnte <bystrov.igor25@gmail.com>
2025-04-04 03:28:58 +0300

    feat: add Authorized main page

---
+++ src/components/anonim/Anonim.tsx
@@ -0,0 +1,13 @@
+const Anonim = () => {
+  return (
+    <div>
+      <p>Welcome</p>
+      <div>
+        <button>Sign In</button>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Anonim;
