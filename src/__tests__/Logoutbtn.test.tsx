8d73994005f17e637a9d82eea3de803c610a7d72
k0nnte <bystrov.igor25@gmail.com>
2025-04-08 19:42:03 +0300

    fix: tets Logoutbtn

--- src/__tests__/Logoutbtn.test.tsx
+++ src/__tests__/Logoutbtn.test.tsx
@@ -2,15 +2,24 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, test, vi } from 'vitest';
 import Logoutbtn from '../components/Header/Logoutbtn';
 import { signOut } from 'firebase/auth';
+import { MemoryRouter } from 'react-router';
 
 vi.mock('firebase/auth', () => ({
   getAuth: vi.fn(() => ({ currentUser: null })),
   signOut: vi.fn(() => Promise.resolve()),
 }));
 
+vi.mock('useNavigate', () => ({
+  useNavigate: vi.fn(),
+}));
+
 describe('logoutbtn', () => {
   test('click logout', () => {
-    render(<Logoutbtn />);
+    render(
+      <MemoryRouter>
+        <Logoutbtn />
+      </MemoryRouter>
+    );
     const button = screen.getByText(/logout/i);
     button.click();
     expect(signOut).toHaveBeenCalled();
@@ -18,7 +27,11 @@ describe('logoutbtn', () => {
 
   test('error', () => {
     vi.mocked(signOut).mockRejectedValueOnce(new Error('error'));
-    render(<Logoutbtn />);
+    render(
+      <MemoryRouter>
+        <Logoutbtn />
+      </MemoryRouter>
+    );
     const button = screen.getByText(/logout/i);
     button.click();
     expect(signOut).toHaveBeenCalled();
