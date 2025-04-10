45943bd0e0b73a6c7b21d4e891fa358ff46c827a
k0nnte <bystrov.igor25@gmail.com>
2025-04-10 09:14:21 +0300

    feat: add privateRoute

--- src/pages/Client.tsx
+++ src/pages/Client.tsx
@@ -1,20 +1,15 @@
 import { redirect } from 'react-router-dom';
 import { isAuth } from '../services/auth';
-import { useLocation } from 'react-router';
-import Rest from './Rest';
+import { Outlet } from 'react-router';
 
 export async function clientLoader() {
   const isLogged = await isAuth();
 
   if (!isLogged) {
-    console.log('не');
-
     throw redirect('/');
   }
 }
 
 export default function SignIn() {
-  const location = useLocation();
-  const isRest = location.pathname.includes('/rest');
-  return isRest ? <Rest /> : <div>s</div>;
+  return <Outlet />;
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,38 @@ export default [
   route('signin', './pages/SignInPage.tsx'),
   route('signup', './pages/SignUpPage.tsx'),
   // route('client', './pages/Client.tsx'),
-  route('rest', './pages/Client.tsx'),
+  route(null, './pages/Client.tsx', [route('rest', './pages/Rest.tsx')]),
+
   route('/*', './pages/ErrorPage.tsx'),
 ] satisfies RouteConfig;
+
+// import { Route, Routes } from 'react-router';
+// import ErrorPage from './pages/ErrorPage';
+// import MainPage from './pages/MainPage';
+// import Rest from './pages/Rest';
+// import SignIn from './pages/SignInPage';
+// import SignUp from './pages/SignUpPage';
+// const AppRoutes = () => {
+//   const navigationRoutes = [
+//     {
+//       path: '/',
+//       element: <MainPage />,
+//     },
+//     { path: '/singin', element: <SignIn /> },
+//     { path: '/signup', element: <SignUp /> },
+//     { path: '/rest', element: <Rest /> },
+//     { path: '/*', element: <ErrorPage /> },
+//   ];
+
+//   return (
+//     <Routes>
+//       {navigationRoutes.map((route) => {
+//         return (
+//           <Route key={route.path} path={route.path} element={route.element} />
+//         );
+//       })}
+//     </Routes>
+//   );
+// };
+
+// export default AppRoutes;
